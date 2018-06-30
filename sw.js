e2d179b088c2ff6ad9615d9e202e400adeec87d1
Owden Godson <owdeng@gmail.com>
2018-06-30 14:49:33 +0300

    removed caching vendor folder from sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Install the Service Worker
-if (self.skipWaiting) { self.skipWaiting(); }
-self.addEventListener('install', function(event) {
+if (skipWaiting) { skipWaiting(); }
+addEventListener('install', function(event) {
 
     event.waitUntil(
         // Open cache
@@ -9,23 +9,44 @@ self.addEventListener('install', function(event) {
             return cache.addAll([
                 '/',
                 'js/',
-                'vendor/',
                 'img/',
-                'css/'
+                'css/',
+                'https://free.currencyconverterapi.com/'
             ]);
         })
     );
 });
 
 
-self.addEventListener('fetch', function(event) {
+addEventListener('fetch', function(event) {
 
     console.log(event.request);
     event.respondWith(
         //If there's a request in the caches that matches
         caches.match(event.request).then(function(response) {
             //If the request is not null, return it, otherwise return the control to the network fetch
-            return response || fetch(event.request);
+            console.log(response);
+            return response || fetch(event.request)     //fetch from internet
+                    .then(function(res) {
+                        return caches.open('cc-sw')
+                            .then(function(cache) {
+                                cache.put(event.request.url, res.clone());    //save the response for future
+                                return res;   // return the fetched data
+                            })
+                    })
+                    .catch(function(err) {       // fallback
+                        return caches.open('cc-sw')
+                            .then(function(cache) {
+                                return cache.match('/index.html');
+                            });
+                    });
         })
     );
+
+
+
+
 });
+
+
+
