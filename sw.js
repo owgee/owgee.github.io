8dbec913b026a38db1a547823788b11c9737e289
Owden Godson <owdeng@gmail.com>
2018-06-30 19:07:50 +0300

    Updated service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 // Install the Service Worker AND Activate it using the activate method
-// Shorthand identifier mapped to specific versioned cache.
 
-var CACHE_VERSION = 1;
-var CURRENT_CACHES = {
+var CACHE_VERSION = 2;
+var ACTIVE_CACHES = {
     all: 'cc-sw' + CACHE_VERSION
 };
 
 //
 self.addEventListener('activate', function(event) {
-    var expectedCacheNames = Object.values(CURRENT_CACHES);
+    var expectedCacheNames = Object.values(ACTIVE_CACHES);
 
     // Active worker won't be treated as activated until promise
     // resolves successfully.
@@ -17,7 +16,7 @@ self.addEventListener('activate', function(event) {
             return Promise.all(
                 cacheNames.map(function(cacheName) {
                     if (!expectedCacheNames.includes(cacheName)) {
-                        console.log('Deleting out of date cache:', cacheName);
+                        console.log('Deleting expired cache...', cacheName);
 
                         return caches.delete(cacheName);
                     }
@@ -27,13 +26,15 @@ self.addEventListener('activate', function(event) {
     );
 });
 
+
+//All magic happens here, I mean, a lot of magic :-/
 self.addEventListener('fetch', function(event) {
     console.log('Handling fetch event for', event.request.url);
 
     event.respondWith(
 
-        // Open All Cache objects that start with 'cc'.
-        caches.open(CURRENT_CACHES['all']).then(function(cache) {
+        // Open Cache object 'all'.
+        caches.open(ACTIVE_CACHES['all']).then(function(cache) {
             return cache.match(event.request).then(function(response) {
                 if (response) {
                     console.log('Found response in cache:', response);
@@ -41,7 +42,7 @@ self.addEventListener('fetch', function(event) {
                     return response;
                 }
 
-                console.log('Fetching request from the network');
+                console.log('Fetching request from the network...');
 
                 return fetch(event.request).then(function(networkResponse) {
                     cache.put(event.request, networkResponse.clone());
@@ -50,7 +51,7 @@ self.addEventListener('fetch', function(event) {
                 });
             }).catch(function(error) {
 
-                // Handles exceptions that arise from match() or fetch().
+                // Handle exceptions that arise from match() or fetch() functions.
                 console.error('Error in fetch handler:', error);
 
                 throw error;
